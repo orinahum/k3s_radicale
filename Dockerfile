d1bdee0c9a92be2777ae0b647fd550827ba39d4f
avishay <avishay@master.localdomain>
2024-09-22 23:51:27 +0300

    Starting the work

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 # Use a small image for Radicale
 FROM python:3.9-alpine
 
-ARG VERSION=3.2.3
-ENV RADICALE_USER=user RADICALE_PASS=changeme
+ARG VERSION='temp_content'
+ENV RADICALE_USER=user RADICALE_PASS=pass
 
 # Install dependencies and Radicale
-RUN apk add --no-cache ca-certificates openssl \
-    && apk add --no-cache --virtual .build-deps gcc libffi-dev musl-dev \
-    && pip install --no-cache-dir "Radicale[bcrypt] @ https://github.com/Kozea/Radicale/archive/${VERSION}.tar.gz" \
-    && apk del .build-deps
+RUN apk add --no-cache gcc musl-dev libffi-dev ca-certificates openssl \
+    && pip install --no-cache-dir "Radicale[bcrypt,md5] @ https://github.com/Kozea/Radicale/archive/${VERSION}.tar.gz" \
+    && pip install passlib[bcrypt] \
+    && apk del gcc musl-dev libffi-dev
 
 # Volumes for config and data
 VOLUME /var/lib/radicale /config
--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+latest=3.2.3
+stable=3.2.2
+test=3.2.1
 # Docker Hub repository
 DOCKER_USERNAME="orinahum1982"
 IMAGE_NAME="radicale"
@@ -8,29 +10,10 @@ DOCKER_TOKEN=""
 # Login Docker Hub
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Versions
-VERSIONS=("latest" "stable" "test")
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:latest . --build-arg VERSION=$latest
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:stable. --build-arg VERSION=$stable
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:test . --build-arg VERSION=$test
 
-# Loop versions and build/push
-for VERSION in "${VERSIONS[@]}"
-do
-    echo "Building and pushing $VERSION version..."
-
-    # Dockerfile based on version
-    if [ "$VERSION" == "latest" ]; then
-        DOCKERFILE="Dockerfile-latest"
-    elif [ "$VERSION" == "stable" ]; then
-        DOCKERFILE="Dockerfile-stable"
-    else
-        DOCKERFILE="Dockerfile-test"
-    fi
-
-    # Build image
-    docker build -f $DOCKERFILE -t $DOCKER_USERNAME/$IMAGE_NAME:$VERSION .
-
-    # Push image
-    docker push $DOCKER_USERNAME/$IMAGE_NAME:$VERSION
-done
 
 # Logout Docker Hub
 docker logout
