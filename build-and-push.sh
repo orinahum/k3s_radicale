ecfcdff7d0c48c1bf14c52929746f1896b9c1dfe
Ori Nahum <ori@ip-192-168-1-132.eu-west-1.compute.internal>
2024-09-26 22:04:23 +0300

    fix radicale configs

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,20 +6,20 @@ test=master
 # Docker Hub repository
 DOCKER_USERNAME="orinahum1982"
 IMAGE_NAME="radicale"
-DOCKER_TOKEN=""
+DOCKER_TOKEN=$(cat dockerhub_token_encoded | base64 --decode)
 
 # Login Docker Hub
-# echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build and tag images for different versions
-docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:latest --build-arg VERSION=$latest .
-docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:stable --build-arg VERSION=$stable .
-docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:test --build-arg VERSION=$test .
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:latest --build-arg VERSION=$latest . --no-cache
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:stable --build-arg VERSION=$stable . --no-cache
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:test --build-arg VERSION=$test . --no-cache
 
 # # Push the images to Docker Hub
-# docker push $DOCKER_USERNAME/$IMAGE_NAME:latest
-# docker push $DOCKER_USERNAME/$IMAGE_NAME:stable
-# docker push $DOCKER_USERNAME/$IMAGE_NAME:test
+docker push $DOCKER_USERNAME/$IMAGE_NAME:latest
+docker push $DOCKER_USERNAME/$IMAGE_NAME:stable
+docker push $DOCKER_USERNAME/$IMAGE_NAME:test
 
 # # Logout Docker Hub
-# docker logout
+docker logout
