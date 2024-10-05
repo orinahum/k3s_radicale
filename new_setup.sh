fe135693662a1e9ff8e03b78efd0f2d5f9de6685
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-10-05 21:28:31 +0300

    testing for new_setup.sh

--- new_setup.sh
+++ new_setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Define colors for better visuals
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-CYAN='\033[0;36m'
+# Define colors
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[4;33m'
+CYAN='\033[1;36m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
-# Dynamic versions with defaults
-versions=("latest" "stable" "test")
-build_versions=("${1:-3.2.3}" "${2:-3.2.2}" "${3:-master}")
+# Sleep time and animations
+SLEEP_DURATION=2
+SPINNER="/-\|"
 
 # Docker Hub credentials
 DOCKER_USERNAME="orinahum1982"
@@ -17,32 +20,37 @@ IMAGE_NAME="radicale"
 DOCKER_TOKEN_BASE64="ZGNrcl9wYXRfaklkN3FNVGdKSzQ1dExWMHl2XzVjeG1FZjRnCg=="
 DOCKER_TOKEN=$(echo $DOCKER_TOKEN_BASE64 | base64 --decode)
 
-# Login Docker Hub
+# Dynamic versions with defaults
+VERSIONS=("latest" "stable" "test")
+BUILD_VERSIONS=("${1:-3.2.3}" "${2:-3.2.2}" "${3:-master}")
+
+# Function to show a spinner during long operations
+show_spinner() {
+    local pid=$!
+    local delay=0.1
+    while [ -d /proc/$pid ]; do
+        for i in ${SPINNER}; do
+            printf "\r%s" "$i"
+            sleep $delay
+        done
+    done
+    printf "\r"
+}
+
+# DockerHub login
 login_to_docker() {
-    echo -e "${CYAN}Logging in to Docker Hub...${NC}"
-    echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin >> /dev/null
+    printf "${CYAN}Logging into Docker Hub...${NC}\n"
+    echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin >> /dev/null & show_spinner
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}DockerHub login completed successfully!${NC}"
+        printf "${GREEN}DockerHub login successful!${NC}\n"
     else
-        echo -e "${RED}DockerHub login failed! Please check your credentials.${NC}"
+        printf "${RED}DockerHub login failed! Please check your credentials.${NC}\n"
         exit 1
     fi
-    sleep 2
+    sleep $SLEEP_DURATION
 }
 
-# Function to display a professional banner
-banner() {
-    echo -e "${CYAN}"
-    echo "############################################"
-    echo "#                                          #"
-    echo "#   RADICALE - AUTO-SCALED APPLICATION     #"
-    echo "#          USING DOCKER & KUBERNETES       #"
-    echo "#                                          #"
-    echo "############################################"
-    echo -e "${NC}"
-}
-
-# General function to find/replace and deploy K8s resources
+# Function to find/replace and deploy Kubernetes resources
 find_replace_and_apply() {
     local version=$1
     local port=$2
@@ -50,17 +58,16 @@ find_replace_and_apply() {
     sed "s/V_PH/${version}/g" deployment_tmp.yaml > k8s/deployment.yaml
     sed "s/V_PH/${version}/g" service_tmp.yaml > k8s/service.yaml
     sed -i "s/V_PH/${port}/g" k8s/service.yaml
-
-    echo -e "${CYAN}Applying Kubernetes resources for version ${YELLOW}${version}${NC} on port ${YELLOW}${port}${NC}..."
+    printf "${CYAN}Deploying version ${YELLOW}${version}${NC} on port ${YELLOW}${port}${NC}...\n"
     kubectl apply -f ./k8s/pv.yaml
     kubectl apply -f ./k8s/pvc.yaml
     kubectl apply -f ./k8s/service.yaml
     kubectl apply -f ./k8s/ingress.yaml
     kubectl apply -f ./k8s/deployment.yaml
-
-    echo -e "${GREEN}Kubernetes deployment completed for version ${YELLOW}${version}${NC}${GREEN}!${NC}"
+    printf "${GREEN}Deployment of version ${version} completed!${NC}\n"
 }
 
+# Function to delete Kubernetes resources
 find_replace_and_delete() {
     local version=$1
     local port=$2
@@ -68,78 +75,89 @@ find_replace_and_delete() {
     sed "s/V_PH/${version}/g" deployment_tmp.yaml > k8s/deployment.yaml
     sed "s/V_PH/${version}/g" service_tmp.yaml > k8s/service.yaml
     sed -i "s/V_PH/${port}/g" k8s/service.yaml
-
-    echo -e "${CYAN}Deleting Kubernetes resources for version ${YELLOW}${version}${NC}..."
+    printf "${CYAN}Deleting version ${YELLOW}${version}${NC}...\n"
     kubectl delete -f ./k8s/deployment.yaml    
     kubectl delete -f ./k8s/service.yaml
     kubectl delete -f ./k8s/ingress.yaml
     kubectl delete -f ./k8s/pvc.yaml
     kubectl delete -f ./k8s/pv.yaml
-
-    echo -e "${GREEN}Kubernetes cleanup completed for version ${YELLOW}${version}${NC}${GREEN}!${NC}"
+    printf "${GREEN}Cleanup of version ${version} completed!${NC}\n"
 }
 
-# General function to build and push Docker image
+# Function to build and push Docker images
 build_and_push() {
     local version=$1
     local build_arg=$2
 
-    echo -e "${CYAN}Building and pushing Docker image for version ${YELLOW}${version}${NC}..."
+    printf "${CYAN}Building and pushing Docker image for version ${YELLOW}${version}${NC}...\n"
     docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:$version --build-arg VERSION=$build_arg . --no-cache
+    docker push $DOCKER_USERNAME/$IMAGE_NAME:$version
     if [ $? -eq 0 ]; then
-        docker push $DOCKER_USERNAME/$IMAGE_NAME:$version
-        echo -e "${GREEN}Docker image pushed successfully for version ${YELLOW}${version}${NC}${GREEN}!${NC}"
+        printf "${GREEN}Docker image ${version} built and pushed successfully!${NC}\n"
     else
-        echo -e "${RED}Failed to build Docker image for version ${YELLOW}${version}${NC}${RED}!${NC}"
+        printf "${RED}Failed to build or push Docker image ${version}.${NC}\n"
     fi
 }
 
-# Loop through the versions and perform actions
+# Function to handle version operations
 handle_version_operations() {
     local action=$1
-    for i in "${!versions[@]}"; do
-        version="${versions[$i]}"
-        build_version="${build_versions[$i]}"
+    for i in "${!VERSIONS[@]}"; do
+        version="${VERSIONS[$i]}"
+        build_version="${BUILD_VERSIONS[$i]}"
 
         if [ "$action" == "push" ]; then
             build_and_push "$version" "$build_version"
         elif [ "$action" == "apply" ]; then
-            find_replace_and_apply ${versions[$i]} "$((80 + i * 10))"
+            find_replace_and_apply ${version} "$((80 + i * 10))"
         elif [ "$action" == "delete" ]; then
-            find_replace_and_delete ${versions[$i]} "$((80 + i * 10))"
+            find_replace_and_delete ${version} "$((80 + i * 10))"
         fi
     done
 }
 
-# Menu logic with better user interaction
+# Menu logic with a clean, professional look
 while true; do
     clear
-    banner
-    echo -e "${YELLOW}                      Configuring Docker Images${NC}"
-    echo -e "[1] Build and Push version Latest         [2] Build and Push version Stable"
-    echo -e "[3] Build and Push version Test           [4] Build and Push all versions"
-    echo -e ""
-    echo -e "${CYAN}                        Kubernetes Configuration${NC}"
-    echo -e "[5] Apply version Latest                  [6] Apply version Stable"
-    echo -e "[7] Apply version Test                    [8] Apply all versions"
-    echo -e ""
-    echo -e "${RED}                              Cleanup${NC}"
-    echo -e "[9] Delete All Radicale Kubernetes Infrastructure"
-    echo -e "[0] Exit Radicale Installation Script"
+    printf "\
+         ███████████                 █████  ███                     ████              ██████████                     █████                            █████   ████  ████████    █████████ 
+        ░░███░░░░░███               ░░███  ░░░                     ░░███             ░░███░░░░███                   ░░███                            ░░███   ███░  ███░░░░███  ███░░░░░███
+         ░███    ░███   ██████    ███████  ████   ██████   ██████   ░███   ██████     ░███   ░░███  ██████   ██████  ░███ █████  ██████  ████████     ░███  ███   ░███   ░███ ░███    ░░░ 
+         ░██████████   ░░░░░███  ███░░███ ░░███  ███░░███ ░░░░░███  ░███  ███░░███    ░███    ░███ ███░░███ ███░░███ ░███░░███  ███░░███░░███░░███    ░███████    ░░████████  ░░█████████ 
+         ░███░░░░░███   ███████ ░███ ░███  ░███ ░███ ░░░   ███████  ░███ ░███████     ░███    ░███░███ ░███░███ ░░░  ░██████░  ░███████  ░███ ░░░     ░███░░███    ███░░░░███  ░░░░░░░░███
+         ░███    ░███  ███░░███ ░███ ░███  ░███ ░███  ███ ███░░███  ░███ ░███░░░      ░███    ███ ░███ ░███░███  ███ ░███░░███ ░███░░░   ░███         ░███ ░░███  ░███   ░███  ███    ░███
+         █████   █████░░████████░░████████ █████░░██████ ░░████████ █████░░██████     ██████████  ░░██████ ░░██████  ████ █████░░██████  █████        █████ ░░████░░████████  ░░█████████ 
+        ░░░░░   ░░░░░  ░░░░░░░░  ░░░░░░░░ ░░░░░  ░░░░░░   ░░░░░░░░ ░░░░░  ░░░░░░     ░░░░░░░░░░    ░░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░        ░░░░░   ░░░░  ░░░░░░░░    ░░░░░░░░░  
 
-    read -p "Choose an option: " p
+
+        ${YELLOW}SETUP AUTO-SCALED RADICALE USING DOCKER AND KUBERNETES${NC}\n
+        ${WHITE}Latest:${BUILD_VERSIONS[0]} \t Stable:${BUILD_VERSIONS[1]} \t Test:${BUILD_VERSIONS[2]} ${NC}\n
+        ${CYAN}Configuring Docker Images:${NC}
+        [${RED}1${NC}] Build & Push Latest  [${RED}2${NC}] Build & Push Stable
+        [${RED}3${NC}] Build & Push Test \t [${RED}4${NC}] Build & Push all versions\n
+        ${GREEN}Kubernetes Configuration:${NC}
+        [${RED}5${NC}] Apply Latest \t [${RED}6${NC}] Apply Stable
+        [${RED}7${NC}] Apply Test \t\t [${RED}8${NC}] Apply all versions\n
+        ${MAGENTA}Cleanup:${NC}
+        [${RED}9${NC}] Delete All Radicale Kubernetes Infrastructure
+        [${RED}0${NC}] Exit Script
+        
+        ${WHITE}Please enter your selection...${NC} "
+
+    read -n1 choice
     clear
-    case "$p" in
-        1) build_and_push ${versions[0]} "${build_versions[0]}"; sleep 2;;
-        2) build_and_push ${versions[1]} "${build_versions[1]}"; sleep 2;;
-        3) build_and_push ${versions[2]} "${build_versions[2]}"; sleep 2;;
-        4) handle_version_operations "push"; sleep 2;;
-        5) find_replace_and_apply ${versions[0]} "80"; sleep 2;;
-        6) find_replace_and_apply ${versions[1]} "90"; sleep 2;;
-        7) find_replace_and_apply ${versions[2]} "100"; sleep 2;;
-        8) handle_version_operations "apply"; sleep 2;;
-        9) handle_version_operations "delete"; sleep 2;;
-        0) docker logout >> /dev/null && echo -e "${GREEN}Logged out from DockerHub. Exiting...${NC}"; exit 0;;
-        *) echo -e "${RED}Invalid option. Please choose a number between 0 and 9.${NC}"; sleep 2;;
+
+    case "$choice" in
+        1) build_and_push "${VERSIONS[0]}" "${BUILD_VERSIONS[0]}"; sleep $SLEEP_DURATION;;
+        2) build_and_push "${VERSIONS[1]}" "${BUILD_VERSIONS[1]}"; sleep $SLEEP_DURATION;;
+        3) build_and_push "${VERSIONS[2]}" "${BUILD_VERSIONS[2]}"; sleep $SLEEP_DURATION;;
+        4) handle_version_operations "push"; sleep $SLEEP_DURATION;;
+        5) find_replace_and_apply "${VERSIONS[0]}" "80"; sleep $SLEEP_DURATION;;
+        6) find_replace_and_apply "${VERSIONS[1]}" "90"; sleep $SLEEP_DURATION;;
+        7) find_replace_and_apply "${VERSIONS[2]}" "100"; sleep $SLEEP_DURATION;;
+        8) handle_version_operations "apply"; sleep $SLEEP_DURATION;;
+        9) handle_version_operations "delete"; sleep $SLEEP_DURATION;;
+        0) docker logout >> /dev/null && printf "${GREEN}Logged out from DockerHub. Exiting...${NC}\n"; exit 0;;
+        *) printf "${RED}Invalid option. Please choose a number between 0 and 9.${NC}\n"; sleep $SLEEP_DURATION;;
     esac
 done
--- setup_new.sh
+++ setup_new.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#
+S="sleep 2"
+
 # Dynamic versions with defaults
-versions=("latest" "stable" "test")
-build_versions=("${1:-3.2.3}" "${2:-3.2.2}" "${3:-master}")
+VERSIONS=("latest" "stable" "test")
+BUILD_VERSIONS=("${1:-3.2.3}" "${2:-3.2.2}" "${3:-master}")
 
 # Docker Hub credentials
 DOCKER_USERNAME="orinahum1982"
@@ -13,7 +16,7 @@ DOCKER_TOKEN=$(echo $DOCKER_TOKEN_BASE64 | base64 --decode)
 # Login Docker Hub
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin >> /dev/null
 echo "DockerHub login completed"  
-sleep 2
+$S
 
 # General function to find/replace and deploy K8s resources
 function find_replace_and_apply() {
@@ -61,24 +64,24 @@ function build_and_push() {
 # Loop through the versions and perform actions
 function handle_version_operations() {
     local action=$1
-    for i in "${!versions[@]}"; do
-        version="${versions[$i]}"
-        build_version="${build_versions[$i]}"
+    for i in "${!VERSIONS[@]}"; do
+        version="${VERSIONS[$i]}"
+        build_version="${BUILD_VERSIONS[$i]}"
 
         if [ "$action" == "push" ]; then
             build_and_push "$version" "$build_version"
         elif [ "$action" == "apply" ]; then
-            find_replace_and_apply ${versions[0]} "80"
-            find_replace_and_apply ${versions[1]} "90"
-            find_replace_and_apply ${versions[2]} "100"
+            find_replace_and_apply ${VERSIONS[0]} "80"
+            find_replace_and_apply ${VERSIONS[1]} "90"
+            find_replace_and_apply ${VERSIONS[2]} "100"
         elif [ "$action" == "delete" ]; then
-            find_replace_and_delete ${versions[0]} "80"
-            find_replace_and_delete ${versions[1]} "90"
-            find_replace_and_delete ${versions[2]} "100"
+            find_replace_and_delete ${VERSIONS[0]} "80"
+            find_replace_and_delete ${VERSIONS[1]} "90"
+            find_replace_and_delete ${VERSIONS[2]} "100"
         fi
     done
 }
-s="sleep 2"
+
 # Menu logic
 while true; do
 printf '\n%s\n' \
@@ -98,17 +101,17 @@ printf '\n%s\n' \
 
     clear
     case "$p" in
-        1) build_and_push ${versions[0]} "${build_versions[0]}" && $s;;
-        2) build_and_push ${versions[1]} "${build_versions[1]}" && $s;;
-        3) build_and_push ${versions[2]} "${build_versions[2]}" && $s;;
-        4) handle_version_operations "push" && $s;;
-        5) find_replace_and_apply ${versions[0]} "80" && $s;;
-        6) find_replace_and_apply ${versions[1]} "90" && $s;;
-        7) find_replace_and_apply ${versions[2]} "100" && $s;;
-        8) handle_version_operations "apply" && $s;;
-        9) handle_version_operations "delete" && $s;;
+        1) build_and_push ${VERSIONS[0]} "${BUILD_VERSIONS[0]}" && $S;;
+        2) build_and_push ${VERSIONS[1]} "${BUILD_VERSIONS[1]}" && $S;;
+        3) build_and_push ${VERSIONS[2]} "${BUILD_VERSIONS[2]}" && $S;;
+        4) handle_version_operations "push" && $S;;
+        5) find_replace_and_apply ${VERSIONS[0]} "80" && $S;;
+        6) find_replace_and_apply ${VERSIONS[1]} "90" && $S;;
+        7) find_replace_and_apply ${VERSIONS[2]} "100" && $S;;
+        8) handle_version_operations "apply" && $S;;
+        9) handle_version_operations "delete" && $S;;
         0) docker logout >> /dev/null && exit 0;;
-        *) echo "[-] Not a valid character. Choose 0-9 " && $s;;
+        *) echo "[-] Not a valid character. Choose 0-9 " && $S;;
     esac
     
 done
